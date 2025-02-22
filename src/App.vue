01a5b39b9dcff57138f42141a8319102d3c11faf
Yurii Rybachok <drummer2009@ukr.net>
2025-02-22 11:58:56 +0200

    add templay for calculator & array for btns

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <script setup>
+import { ref } from "vue";
 
+const display = ref('');
+const buttons = [
+  { value: 'AC', type: 'clear' },
+  { value: 'DEL', type: 'delete' },
+  { value: '.', type: 'operator' },
+  { value: '/', type: 'operator' },
+
+  { value: '7' },
+  { value: '8' },
+  { value: '9' },
+  { value: '*', type: 'operator' },
+
+  { value: '4' },
+  { value: '5' },
+  { value: '6' },
+  { value: '-', type: 'operator' },
+
+  { value: '1' },
+  { value: '2' },
+  { value: '3' },
+  { value: '+', type: 'operator' },
+
+  { value: '000' },
+  { value: '00' },
+  { value: '0' },
+  { value: '=', type: 'equals' },
+];
 </script>
 
 <template>
-  <span>
-    123
-  </span>
+  <div class="calculator">
+    <input type="text" v-model="display" class="display" disabled />
+    <div class="button"></div>
+  </div>
 </template>
